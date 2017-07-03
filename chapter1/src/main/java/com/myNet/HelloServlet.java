6b073e38a44f14fbba599f721f20b1d5aa7589ee
moon <837669872@qq.com>
2017-07-03 15:17:16 +0800

    startAngle

---
+++ chapter1/src/main/java/com/myNet/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.myNet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Created by moon on 7/3 0003.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("time",new Date());
+        req.getRequestDispatcher("/WEB-INF/jsp/timePage.jsp").forward(req,resp);
+        super.doGet(req, resp);
+    }
+
+
+}
---
+++ chapter1/src/main/webapp/WEB-INF/jsp/timePage.jsp
@@ -0,0 +1,5 @@
+<%@ page pageEncoding="UTF-8" %>
+<html>
+    <h1>title</h1>
+<span>${time}</span>
+</html>
